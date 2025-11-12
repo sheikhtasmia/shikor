8d3df2000fb8a1d07b0058366520b61be83056b3
Tasmia Eshrak Jhumu <240708959+iamtasmia@users.noreply.github.com>
2025-11-12 23:55:39 +0600

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-green-700 text-white text-center py-6">
+      <p>© {new Date().getFullYear()} Shikor.org — All Rights Reserved.</p>
+    </footer>
+  );
+}
