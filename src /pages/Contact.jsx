679aa57054497e6e89c3da441db167c6a5791689
Tasmia Eshrak Jhumu <240708959+iamtasmia@users.noreply.github.com>
2025-11-12 23:51:52 +0600

    Delete src  directory

--- src /pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="py-16 px-6 md:px-20 text-center bg-green-50">
-      <h2 className="text-3xl font-semibold text-green-700 mb-4">Contact Us</h2>
-      <p className="text-gray-700 mb-6">For any queries, please reach out to:</p>
-      <p className="font-medium text-gray-800">📧 info@shikor.org</p>
-    </div>
-  );
-}
